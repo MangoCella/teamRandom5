083fea4f97b557a7fd00c63b20c6e7d99a0c6154
MangoCella <cella@gmx.at>
2023-01-22 21:19:39 +0100

    NewClockUpdate2

--- src/main/java/team/random5/NewClock.java
+++ src/main/java/team/random5/NewClock.java
@@ -3,7 +3,6 @@ package team.random5;
 import javafx.animation.AnimationTimer;
 import javafx.application.Application;
 import javafx.scene.Scene;
-import javafx.scene.layout.HBox;
 import javafx.scene.transform.Rotate;
 import javafx.stage.Stage;
 import javafx.scene.shape.*;
@@ -14,99 +13,119 @@ import javafx.scene.paint.Color;
 import java.util.Calendar;
 
 public class NewClock extends Application {
-        private Line hourHand;
-        private Line minuteHand;
-        private Line secondHand;
-        double clockRadius;
-        double centerX;
-        double centerY;
+    private Line hourHand;
+    private Line minuteHand;
+    private Line secondHand;
+    double clockRadius = 110; // added default value for clock radius
+    double centerX;
+    double centerY;
 
-        private Label timeLabel;
-
+    private Label digitimeLabel;
 
 
     @Override
     public void start(Stage primaryStage) {
+        //JavaFX Setup
         Pane pane = new Pane();
         Scene scene = new Scene(pane, 500, 500);
         primaryStage.setTitle("Random");
         primaryStage.setScene(scene);
         primaryStage.show();
 
+
+        // Big Circle as Base
         Circle clockCircle = new Circle();
         clockCircle.centerXProperty().bind(pane.widthProperty().divide(2));
         clockCircle.centerYProperty().bind(pane.heightProperty().divide(2));
-        clockCircle.setStroke(Color.CHOCOLATE);
-        clockCircle.setFill(Color.DARKMAGENTA);
+        clockCircle.setRadius(clockRadius); // added radius to the clockCircle
+        clockCircle.setFill(Color.TRANSPARENT);
         pane.getChildren().add(clockCircle);
+        centerX = clockCircle.getCenterX();
+        centerY = clockCircle.getCenterY();
+
+
+        // Big Circle
+        Circle circle = new Circle();
+        circle.setCenterX(centerX+5);
+        circle.setCenterY(centerY+9);
+        circle.setRadius(120);
+        circle.setFill(Color.DARKGRAY);
+        pane.getChildren().add(circle);
 
         // Draw clock ticks
         for (int i = 0; i < 12; i++) {
             double angle = i * 30;
-            double x = centerX + clockRadius * Math.sin(Math.toRadians(angle));
-            double y = centerY - clockRadius * Math.cos(Math.toRadians(angle));
+            double x = clockCircle.getCenterX() + clockCircle.getRadius() * Math.sin(Math.toRadians(angle));
+            double y = clockCircle.getCenterY() - clockCircle.getRadius() * Math.cos(Math.toRadians(angle));
             String tickLabel = String.valueOf(i == 0 ? 12 : i);
             Label label = new Label(tickLabel);
+            //label.setFont(Font.font("Helvetica"));
             label.setLayoutX(x - label.getWidth() / 2);
             label.setLayoutY(y - label.getHeight() / 2);
             pane.getChildren().add(label);
         }
 
-
-        Circle clockNub = new Circle();
-        clockNub.centerXProperty().bind(pane.widthProperty().divide(2));
-        clockNub.centerYProperty().bind(pane.heightProperty().divide(2));
-        clockNub.setRadius(10);
-        clockNub.setFill(Color.DIMGREY);
-        pane.getChildren().add(clockNub);
-
-        // Hands with rounded edges
-        // Hour hand
-        hourHand.setStroke(Color.CHOCOLATE);
+        // Create clock hands:
+        // Hours hand
+        hourHand = new Line();
+        hourHand.setStroke(Color.WHITE);
         hourHand.setStrokeWidth(8);
-        double handLength = clockRadius * 0.6;
-        double handWidth = 10;
-
-        MoveTo start = new MoveTo(centerX, centerY);
-        LineTo line1 = new LineTo(centerX, centerY - handLength + handWidth / 2);
-        ArcTo arc1 = new ArcTo(handWidth / 2, handWidth / 2, 0, centerX + handWidth / 2, centerY - handLength, false, true);
-        LineTo line2 = new LineTo(centerX + handWidth / 2, centerY);
-        ArcTo arc2 = new ArcTo(handWidth / 2, handWidth / 2, 0, centerX - handWidth / 2, centerY, false, true);
-
+        hourHand.setStrokeLineCap(StrokeLineCap.ROUND);
+        hourHand.setStartX(centerX);
+        hourHand.setStartY(centerY);
+        hourHand.setEndX(centerX);
+        hourHand.setEndY(centerY - clockRadius * 0.6);
         pane.getChildren().add(hourHand);
 
         // Minute hand
-        minuteHand.setStroke(Color.BLUE);
+        minuteHand = new Line();
+        minuteHand.setStroke(Color.WHITE);
         minuteHand.setStrokeWidth(6);
-        double minuteHandLength = clockRadius * 0.9;
-        double minuteHandWidth = 10;
+        minuteHand.setStrokeLineCap(StrokeLineCap.ROUND);
+        minuteHand.setStartX(centerX);
+        minuteHand.setStartY(centerY);
+        minuteHand.setEndX(centerX);
+        minuteHand.setEndY(centerY - clockRadius * 0.8);
+        pane.getChildren().add(minuteHand);
 
-        MoveTo mStart = new MoveTo(centerX, centerY);
-        LineTo mLine1 = new LineTo(centerX, centerY - minuteHandLength + minuteHandWidth / 2);
-        ArcTo mArc1 = new ArcTo(minuteHandWidth / 2, minuteHandWidth / 2, 0, centerX + minuteHandWidth / 2, centerY - minuteHandLength + minuteHandWidth / 2, false, true);
-        LineTo mLine2 = new LineTo(centerX + minuteHandWidth / 2, centerY);
-        ArcTo mArc2 = new ArcTo(minuteHandWidth / 2, minuteHandWidth / 2, 0, centerX - minuteHandWidth / 2, centerY, false, true);
-
-        secondHand.setStroke(Color.DARKRED);
-        secondHand.setStrokeWidth(6);
-        double secondHandLength = clockRadius * 0.9;
-        double secondHandWidth = 3;
-
-        MoveTo sStart = new MoveTo(centerX, centerY);
-        LineTo sLine1 = new LineTo(centerX, centerY - secondHandLength + secondHandWidth / 2);
-        ArcTo sArc1 = new ArcTo(secondHandWidth / 2, secondHandWidth / 2, 0, centerX + secondHandWidth / 2, centerY - secondHandLength + secondHandWidth / 2, false, true);
-        LineTo sLine2 = new LineTo(centerX + secondHandWidth / 2, centerY);
-        ArcTo sArc2 = new ArcTo(secondHandWidth / 2, secondHandWidth / 2, 0, centerX - secondHandWidth / 2, centerY, false, true);
+        // Second hand
+        secondHand = new Line();
+        secondHand.setStroke(Color.TOMATO);
+        secondHand.setStrokeWidth(4);
+        secondHand.setStrokeLineCap(StrokeLineCap.ROUND);
+        secondHand.setStartX(centerX);
+        secondHand.setStartY(centerY);
+        secondHand.setEndX(centerX);
+        secondHand.setEndY(centerY - clockRadius * 0.9);
         pane.getChildren().add(secondHand);
 
 
-// Create a label to display the current time
-        timeLabel.setTextFill(Color.CHOCOLATE);
-        timeLabel.setLayoutX(centerX - timeLabel.getWidth() / 2);
-        timeLabel.setLayoutY(centerY + clockRadius + 20);
-        pane.getChildren().add(timeLabel);
+        // Create rotation transforms for clock hands
+        Rotate hourRotation = new Rotate(0, centerX, centerY);
+        hourHand.getTransforms().add(hourRotation);
+        Rotate minuteRotation = new Rotate(0, centerX, centerY);
+        minuteHand.getTransforms().add(minuteRotation);
+        Rotate secondRotation = new Rotate(0, centerX, centerY);
+        secondHand.getTransforms().add(secondRotation);
 
-// Update clock hands and time label every second
+        // Little Circle
+        Circle clockNub = new Circle();
+        clockNub.setCenterX(centerX);
+        clockNub.setCenterY(centerY);
+        clockNub.setRadius(9);
+        clockNub.setFill(Color.LIGHTGRAY);
+        pane.getChildren().add(clockNub);
+
+
+        // Create a label to display the current time in digits
+        digitimeLabel = new Label();
+        double x = clockCircle.getCenterX() * 0.9;
+        double y = clockCircle.getCenterY() * 0.9;
+        digitimeLabel.setLayoutX(x - digitimeLabel.getWidth() / 2);
+        digitimeLabel.setLayoutY(y + clockRadius * 0.7);
+        pane.getChildren().add(digitimeLabel);
+
+        // Create animation timer to update clock hands and time label
         AnimationTimer timer = new AnimationTimer() {
             @Override
             public void handle(long now) {
@@ -115,7 +134,7 @@ public class NewClock extends Application {
                 int minute = cal.get(Calendar.MINUTE);
                 int second = cal.get(Calendar.SECOND);
                 updateClockHands(hour, minute, second);
-                //timeLabel(hour, minute, second);
+                digitimeLabel.setText(String.format("%02d:%02d:%02d", hour, minute, second));
             }
         };
         timer.start();
